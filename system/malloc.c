04d0e9abc017e04db237aa39759d04c891a5665a
William Dignazio <slackwill@csh.rit.edu>
2012-01-28 23:06:56 -0500

    started malloc program

---
+++ system/malloc.c
@@ -0,0 +1,12 @@
+/* Memory Allocation Tool
+** Author: Will Dignazio 
+** Date: 01/28/2012
+** Description: 
+**		Code for tool to allocate
+**	memory based on the free space available
+**	within the physical memory. 
+*/
+
+#include <foundation/system.h>
+
+
