0a0cbed0b32393013ed0c33eaad4cb4be280e5f6
William Dignazio <slackwill@csh.rit.edu>
2012-01-29 13:13:12 -0500

    added stubs for the memory objects

---
+++ system/memstack.c
@@ -0,0 +1,8 @@
+/* Memory Allocation Stack Driver
+** Author: 
+** Date: 01/29/2012
+** Description: 
+** 	 Controls the stack style of 
+** management for malloc and the allocation
+** of memory for variables within processes. 
+*/
